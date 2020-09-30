8b66af129d62ac95b0d95e7e4acfe0b5ac0762a0
aaroncottis <72127408+aaroncottis@users.noreply.github.com>
2020-09-30 23:10:57 +0100

    Created & added main and header files
    Main, functions and header files created and started coding

---
+++ tic_tac_toe.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "tic_tac_toe.hpp"
+
+int main(){
+
+
+   intro();
+   char temp;
+   temp = std::cin.get();
+
+   gridPosition();
+
+}
---
+++ tic_tac_toe_functions.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+void intro(){
+    std::cout << "===============================\n";
+    std::cout << "          TIC TAC TOE          \n";
+    std::cout << "Player 1 is 0 and Player 2 is X\n";
+    std::cout << "         Best of three!        \n";
+    std::cout << "===============================\n\n";
+    std::cout << "Press enter to start: \n";
+    }
+
+void gridPosition(){
+    std::cout << "1 | 2 | 3\n---------\n4 | 5 | 6\n---------\n7 | 8 | 9\n";
+}
